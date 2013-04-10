018beba6d60ffb9cbf0dd236cc0220eb6ac62257
Tracy Moscato <trcmoscato@gmail.com>
2013-04-09 20:57:15 -0400

    Twil_App

--- local_settings.py
+++ local_settings.py
@@ -4,14 +4,14 @@ Configuration Settings
 Includes keys for Twilio, etc.  Second stanza intended for Heroku deployment.
 '''
 
-# Uncommet to configure in file.
+#Uncommet to configure in file.
 ACCOUNT_SID = "ACc6bc4e62ba004615fe9bbcf9b244d2e8"
 AUTH_TOKEN = "600da7b3d7304f152b0525d57b16fd28"
-Twil_App = "AP7372909d77981da2347788c983ad57c0"
+Twil_APP = "AP7372909d77981da2347788c983ad57c0"
 Twil_SPAM_ID = "+1 508-401-7549"
 
 
- #Begin Heroku configuration - configured through environment variables.
+#Begin Heroku configuration - configured through environment variables.
 import os
 ACCOUNT_SID = os.environ['ACCOUNT_SID']
 AUTH_TOKEN = os.environ['AUTH_TOKEN']
